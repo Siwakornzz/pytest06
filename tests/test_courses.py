a234c4f1a4d2b0b52c38b4f92da56624f4526e4f
Siwakornzz <Siwakornzz@hotmail.com>
2022-02-28 20:03:53 +0700

    init commit

---
+++ tests/test_courses.py
@@ -0,0 +1,44 @@
+from fastapi.testclient import TestClient
+import sys        
+sys.path.insert(0, '../Lab05')        
+from main import app
+
+client = TestClient(app)
+
+def test_call_test_api():
+    response = client.get("/courses/test")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
+def test_call_post_api():
+    response = client.post("/courses/post")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
+def test_call_post_data_api():
+    input = "Aun"
+    response = client.post("/courses/body",
+        json = {"name": input}    
+    )
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello "+input}
+
+def test_false_post_data_api():
+    input = "Aun"
+    response = client.post("/courses/body",
+        json = {"name": input}
+        )
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+# def test_post_insert():
+#     response = client.post(
+#         "/students/",
+#         json={
+#             "name": "string",
+#             "description": "string",
+#             "completed": "true",
+#             "date": "string"
+#         }
+#     )
+#     assert response.status_code == 200
+#     assert response.json()[0]["name"] == "string"
